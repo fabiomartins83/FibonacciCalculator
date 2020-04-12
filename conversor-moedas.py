4538f53b296713c12daab0d45ddf764954aae93a
Fábio Martins <fabio.martins.01@hotmail.com>
2020-04-12 04:23:55 -0300

    initial commit

---
+++ conversor-moedas.py
@@ -0,0 +1,7 @@
+# Conversor de moedas
+# Desenvolvido por Fábio de Almeida Martins
+
+print("CONVERSOR DE MOEDAS")
+x = round(float(input("Digite a cotação atual do dólar para R$1,00: ")), 2)
+y = round(float(input("Digite o valor em R$ a ser calculado: ")), 2)
+print("O valor em dólares é US$", round(x * y, 2))
--- fibonacci.py
+++
@@ -1,9 +0,0 @@
-# Calculadora para números de Fibonacci
-# Desenvolvido por Fábio de Almeida Martins
-
-a, b = 0, 1
-x = int(input("CALCULADORA FIBONACCI. Digite o número limite: "))
-print("Calculando os números de Fibonacci até",x,": ")
-while a < x:
-    print(a, end=" ")
-    a, b = b, a+b
